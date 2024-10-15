69f6a71829656ddb586161f4333921bd3dac622c
Antoine Ferron <antoine.ferron6@gmail.com>
2024-10-15 17:08:02 +0200

    style done

---
+++ assets/javascript/filter-bar.js
@@ -0,0 +1,29 @@
+const ingredientDisplaySearch = document.getElementById('displayed-Ingredients-tag-search');
+const ingredientSearchInput = document.getElementById('search-ingredients-label');
+const ingredientTagsWrapperButton = document.getElementById('ingredients-button-wrapper');
+
+const devicesDisplaySearch = document.getElementById('displayed-device-tag-search');
+const devicesSearchInput = document.getElementById('search-devices-label');
+const devicesTagsWrapperButton = document.getElementById('devices-button-wrapper');
+
+const utensilsDisplaySearch = document.getElementById('displayed-utensils-tag-search');
+const utensilsSearchInput = document.getElementById('search-utensils-label');
+const utensilsTagsWrapperButton = document.getElementById('utensils-button-wrapper');
+
+ingredientSearchInput.addEventListener('click', () => {
+    toggleDisplay(ingredientDisplaySearch, ingredientTagsWrapperButton);
+});
+
+devicesSearchInput.addEventListener('click', () => {
+    toggleDisplay(devicesDisplaySearch, devicesTagsWrapperButton);
+});
+
+utensilsSearchInput.addEventListener('click', () => {
+    toggleDisplay(utensilsDisplaySearch, utensilsTagsWrapperButton);
+});
+
+function toggleDisplay(displayElement, buttonElement) {
+    displayElement.style.display = displayElement.style.display === 'block' ? 'none' : 'block';
+    buttonElement.classList.toggle('turn');
+}
+
